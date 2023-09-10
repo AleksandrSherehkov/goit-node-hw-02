024ad26a1cef246b0dabf64909c0735d0a36ddd2
Aleksandr Shereshkov <aleksandr.shereshkov@gmail.com>
2023-09-10 19:23:09 +0300

    ADD:done work hw05-avatars

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const ctrl = require('../../controllers/auth');
 const { validateBody } = require('../../decorators');
-const { authenticate } = require('../../middlewares');
+const { authenticate, upload } = require('../../middlewares');
 
 const schemas = require('../../schemas/users');
 
@@ -17,5 +17,6 @@ router.post('/signin', signinValidateMiddleware, ctrl.signin);
 router.get('/current', authenticate, ctrl.getCurrent);
 router.post('/signout', authenticate, ctrl.signout);
 router.patch('/:id/user', authenticate, updateSubscriptionSchema, ctrl.updateSubscription);
+router.patch('/avatars', authenticate, upload.single('avatar'), ctrl.updateAvatar);
 
 module.exports = router;
